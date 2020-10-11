46d7624c14b745a45674264d0770ec829b2f31f0
zhang xian <1756569043@qq.com>
2020-10-11 15:15:13 +0800

    完成名字修改功能

--- README.md
+++ README.md
@@ -27,7 +27,7 @@
 
 3. srt  字幕文件，只保留 `en.srt` 和 `zh-CN.srt` 结尾的字幕文件，删除其他语言的字幕文件
 
-4. 修改剩下文件的文件名，去掉文件名的空格，根据 course、class、section 的序号，给文件名添加序号。
+4. 修改剩下文件的文件名，去掉文件名的空格，根据 course、class、section 的序号，给文件名添加序号。包括 mp4 文件和 srt 文件。
 
    例子：
    
@@ -40,6 +40,6 @@
    - 修改后的文件名：
    
    ```
-   1 introduction-tensorflow\1_a-new-programming-paradigm\01_a-new-programming-paradigm\1.1.1 introduction a conversation with andrew ng.mp4```
+   1 introduction-tensorflow\1_a-new-programming-paradigm\01_a-new-programming-paradigm\1.1.1 introduction a conversation with andrew ng.mp4
    ```
 
---
+++ rename.py
@@ -0,0 +1,75 @@
+import os
+import glob
+
+
+root_dir = 'D:\coursera'
+
+
+# 源文件名
+# introduction-tensorflow\1_a-new-programming-paradigm\01_a-new-programming-paradigm\01_introduction-a-conversation-with-andrew-ng.mp4
+
+# 目标文件名
+# 1 introduction-tensorflow\1_a-new-programming-paradigm\01_a-new-programming-paradigm\1.1.1 introduction a conversation with andrew ng.mp4
+
+for course in os.listdir(root_dir):
+    course_dir = os.path.join(root_dir, course)
+    if os.path.isdir(course_dir):
+        course_num = course[0:1]
+        coursera_dir = os.path.join(root_dir, course)
+        for classes in os.listdir(coursera_dir):
+            classes_dir = os.path.join(coursera_dir, classes)
+            if os.path.isdir(classes_dir):
+                class_num = classes[0:1]
+                # courses.append(course)
+                for section in os.listdir(classes_dir):
+                    section_dir = os.path.join(classes_dir, section)
+                    if os.path.isdir(section_dir):
+                        section_num = section[0:2]
+                        section_num = section_num[1:2] if section_num.startswith('0') else section_num
+                        # 删除 . 开头的 mp4 文件
+                        mp4_files = glob.glob(os.path.join(section_dir, '*.mp4'))
+                        for mp4_file in mp4_files:
+                            if os.path.basename(mp4_file).startswith('.'):
+                                os.remove(mp4_file)
+
+                        # 删除 txt 文件
+                        txt_files = glob.glob(os.path.join(section_dir, '*.txt'))
+                        for txt_file in txt_files:
+                            os.remove(txt_file)
+
+                        # 只保留 en.srt 文件和 zh-CN.srt 文件，删除其他 srt 文件
+                        srt_files = glob.glob(os.path.join(section_dir, '*.srt'))
+                        for srt_file in srt_files:
+                            if not (srt_file.endswith("en.srt") or srt_file.endswith("zh-CN.srt")):
+                                os.remove(srt_file)
+
+                        # 重新遍历剩下 mp4 文件
+                        mp4_files = glob.glob(os.path.join(section_dir, '*.mp4'))
+                        # 处理文件名
+                        for origin_mp4_file in mp4_files:
+                            (path, origin_file_name) = os.path.split(origin_mp4_file)
+
+                            file_num = origin_file_name[0:2]
+                            file_num = file_num[1:2]  if file_num.startswith('0') else file_num
+                            file_name = ' '.join(origin_file_name.split('_')[1:])
+                            target_file_name = course_num+'.'+class_num+'.'+section_num+'.'+file_num+' '+file_name
+                            target_mp4_file = os.path.join(path, target_file_name)
+                            os.rename(origin_mp4_file, target_mp4_file)
+                            print(target_file_name)
+
+
+
+                        # 重新遍历剩下 srt 文件
+                        srt_files = glob.glob(os.path.join(section_dir, '*.srt'))
+                        # 处理文件名
+                        for origin_srt_file in srt_files:
+                            (path, origin_file_name) = os.path.split(origin_srt_file)
+
+                            file_num = origin_file_name[0:2]
+                            file_num = file_num[1:2]  if file_num.startswith('0') else file_num
+                            file_name = ' '.join(origin_file_name.split('_')[1:])
+                            target_file_name = course_num+'.'+class_num+'.'+section_num+'.'+file_num+' '+file_name
+                            target_srt_file = os.path.join(path, target_file_name)
+                            os.rename(origin_srt_file, target_srt_file)
+                            print(target_file_name)
+
